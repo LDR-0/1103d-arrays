8d32738def27f3e88eb4c79de0dabe8bff608883
LDR-0 <147997685+LDR-0@users.noreply.github.com>
2023-11-08 11:34:04 +0000

    add scrip

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
+  return Array(arraySize).fill(value);
 }
 
-const data = 3;
+const data1 = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(data1, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
+return array.reverse();
  }
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const data2 = [1, 2, 3];
+console.log(reverse(data2)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
+  return array.filter(value => {
+    return value !== false && value !== undefined && value !== '' && value !== 0 && value !== null;
+  });
 }
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
